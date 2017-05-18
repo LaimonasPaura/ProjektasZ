7c784d5580fdda8cfb3db46a121436d399e8bf71
Laimonas_Wayne <nekaskitas@gmail.com>
2017-05-18 21:18:35 +0300

    Sumeciau ka padarem

--- index.php
+++ index.php
@@ -1,127 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Projektas</title>
+	<title>ProjektasZ</title>
 	<meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="css/Gstyle.css">
+    <link rel="stylesheet" type="text/css" href="css/Nstyle.css">
+    <link rel="stylesheet" type="text/css" href="css/Lstyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 </head>
 <body>
-	<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "grupe";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+<section class="nav">
+   <nav>
+    <div class="nav-wrapper">
+      <a href="#!" id="logo" class="brand-logo center">Logo</a>
+      <ul class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li class="active"><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+        
+ <div class="carousel">
+    <a class="carousel-item" href="http://materializecss.com/carousel.html"><img src="http://lorempixel.com/250/250/nature/1"></a>
+    <a class="carousel-item" href="#two!"><img src="http://lorempixel.com/250/250/nature/2"></a>
+    <a class="carousel-item" href="#three!"><img src="http://lorempixel.com/250/250/nature/3"></a>
+    <a class="carousel-item" href="#four!"><img src="http://lorempixel.com/250/250/nature/4"></a>
+    <a class="carousel-item" href="#five!"><img src="http://lorempixel.com/250/250/nature/5"></a>
+  </div>
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-
-$sql = "SELECT id, name, surname, age FROM lentele";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {    //patikrinam kiek stulpeliu!!!!!!!
-    // output data of each row
-
-    echo "<table border='1'>";
-
-    while($row = mysqli_fetch_assoc($result)) {   //visus atsakymus is eiles ikeliam i row, ir su while eilute po eilutes tai padarom
-
-    	echo "<tr>";
-
-    	// //print_r($row);
-    	// echo "<br><br>";
-
-    	echo "<td>" . $row["id"]. "</td>";
-    	echo "<td>" . $row["name"]. "</td>";
-    	echo "<td>" . $row["surname"]. "</td>";
-    	echo "<td>" . $row["age"]. "</td>";
+</section>
 
 
-       // echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["surname"]. " " . $row["age"]. "<br>";
-
-        echo "</tr>";
-    }
-
-    echo "</table>";
-
-} else {
-    echo "0 results";
-}
 
 
-$sql = "SELECT ID, name, message FROM atsiliepimai";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {    //patikrinam kiek stulpeliu!!!!!!!
-    // output data of each row
-
-    echo "<table border='1'>";
-
-    while($row = mysqli_fetch_assoc($result)) {   //visus atsakymus is eiles ikeliam i row, ir su while eilute po eilutes tai padarom
-
-    	echo "<tr>";
-
-    	// //print_r($row);
-    	// echo "<br><br>";
-
-    	echo "<td>" . $row["ID"]. "</td>";
-    	echo "<td>" . $row["name"]. "</td>";
-    	echo "<td>" . $row["message"]. "</td>";
-    	
-
-
-       // echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["surname"]. " " . $row["age"]. "<br>";
-
-        echo "</tr>";
-    }
-
-    echo "</table>";
-
-} else {
-    echo "0 results";
-}
-//mysqli_close($conn);
-?>
-<?php include('header.php'); ?>
-<h1>Labas PHP</h1>
-<?php
-echo "<p>Labas, Seniuk</p>";
-
-if ($_GET) {
-
-
-$name = $_GET['name'];
-
-$message =$_GET['message'] ;
-
-echo "$name";
-echo "<br><br>";
-echo "$message";
-echo "<br><br>";
-$sql = "INSERT INTO atsiliepimai (ID, name, message)
-VALUES ('', '".$name."', ' ".$message."')";
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-
-mysqli_close($conn);
-}
-?>
-<form action= "about.php" method="get">
-	Vardas : <input type="text" name="name">
-
-	atsiliepimai: <textarea name="message"> 
-	</textarea>
-	<input type="submit" value="Irasyti">
-</form>
-
+<script type="text/javascript" src="js/Gscript.js"></script>
+<script type="text/javascript" src="js/Nscript.js"></script>
+<script type="text/javascript" src="js/Lscript.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 </body>
 </html>
