a4363851a6c5d70957cec6c8d652e731c75d5c7b
Laimonas_Wayne <nekaskitas@gmail.com>
2017-05-23 21:32:29 +0300

    antradienio paskutinis pushinimas

--- footer.php
+++ footer.php
@@ -1,25 +1,32 @@
- <footer class="page-footer dark-blue">
+ <footer class="page-footer dark-blue transparency">
   <div class="container">
     <div class="row">
-      <div class="col l6 s12">
+      <!-- <div class="col l6 s12">
         <h5 class="white-text">Footer Content</h5>
         <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="white-text">Links</h5>
-        <ul>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-        </ul>
+      </div> -->
+      <div class="col l8 offset-l0 s12">
+        <h5 class="white-text">Check Us on:</h5>
+        <div class="rounded-social-buttons">
+  <a class="social-button facebook" href="https://www.facebook.com/profile.php?id=100000163254321&ref=br_rs"></a>
+  <a class="social-button twitter" href="#"></a>
+  <a class="social-button linkedin" href="#"></a>
+  <a class="social-button youtube" href="#"></a>
+  <a class="social-button instagram" href="#"></a>
+  <a class="social-button pinterest" href="#"></a>
+  <a class="social-button google-plus" href="#"></a>
+  <a class="social-button github" href="#"></a>
+  <a class="social-button tumblr" href="#"></a>
+</div>
+
       </div>
     </div>
   </div>
   <div class="footer-copyright">
     <div class="container">
-      © 2014 Copyright Text
-      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      © 2014 All rights reserved 
+      <p class= "grey-text text-lighten-4 right"> Mūsų adresas: nežinomas. Telefonas: irgi.</p>
+      <!-- <a class="grey-text text-lighten-4 right" href="#!">More Links</a> -->
     </div>
   </div>
 </footer>
